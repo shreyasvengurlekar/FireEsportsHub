a0ece7be64bb4abec729be82cf1bc94383a44582
shreyasvengurlekar <shreyasvengurlekar2004@gmail.com>
2025-10-02 12:43:10 +0530

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 export default defineConfig({
   plugins: [
@@ -16,6 +18,14 @@ export default defineConfig({
         ]
       : []),
   ],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss(path.resolve(import.meta.dirname, "tailwind.config.ts")),
+        autoprefixer(),
+      ],
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
